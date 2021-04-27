6d8349e82066e01cae7489f0fbd0a215ad71dc0b
Levente Mátyus <levente.matyus@stud.uni-corvinus.hu>
2021-04-27 22:01:22 +0200

    Gy8

--- HajósTeszt/HajósTeszt/Controllers/BoatController.cs
+++ HajósTeszt/HajósTeszt/Controllers/BoatController.cs
@@ -8,24 +8,18 @@ using System.Threading.Tasks;
 
 namespace HajósTeszt.Controllers
 {
-   // [Route("api/[controller]")]
+    // [Route("api/[controller]")]
     [ApiController]
     public class BoatController : ControllerBase
     {
         [HttpGet]
-        [Route("questions/{sorszám}")]
-        public ActionResult M2(int sorszám)
+        [Route("questions/all")]
+        public ActionResult M1()
         {
             HajostesztContext context = new HajostesztContext();
-            var kérdés = (from x in context.Questions
-                          where x.QuestionId == sorszám
-                          select x).FirstOrDefault();
+            var kérdések = from x in context.Questions select x.QuestionText;
 
-            if (kérdés == null) return BadRequest("Nincs ilyen sorszámú kérdés");
-
-            return new JsonResult(kérdés);
-
-
+            return new JsonResult(kérdések);
         }
     }
 }
