8b7dd3e7b92e8a20d6f16d2728aca34842886599
Levente Mátyus <levente.matyus@stud.uni-corvinus.hu>
2021-05-16 09:05:21 +0200

    gy12

---
+++ HajósTeszt/HajósTeszt/Controllers/JokeControlle.cs
@@ -0,0 +1,56 @@
+﻿using HajósTeszt.JokeModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HajósTeszt.Controllers
+{
+    [Route("api/jokes")]
+    [ApiController]
+    public class JokeControlle : ControllerBase
+    {
+        // GET: api/jokes
+        [HttpGet]
+        public IEnumerable<Joke> Get()
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            return context.Jokes.ToList();
+        }
+
+        // GET api/jokes/5
+        [HttpGet("{id}")]
+        public Joke Get(int id)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            var keresettVicc = (from x in context.Jokes
+                                where x.JokeSk == id
+                                select x).FirstOrDefault();
+            return keresettVicc;
+        }
+
+        // POST api/jokes
+        [HttpPost]
+        public void Post([FromBody] Joke újVicc)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            context.Jokes.Add(újVicc);
+            context.SaveChanges();
+        }
+
+        // PUT api/<JokeControlle>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<JokeControlle>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
